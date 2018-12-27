ad8e15131686b051057717077f28d92c5e9ad883
BlackSpotBear <blackspotbear@users.noreply.github.com>
2018-12-27 12:28:52 +0900

    Patch box2dweb to avoid infinite loop and long calculation time

--- src/Box2D.ts
+++ src/Box2D.ts
@@ -2,6 +2,230 @@ import * as b2 from "box2dweb";
 import {BodyType} from "./BodyType";
 import * as options from "./Box2DOptions";
 
+/* tslint:disable */
+// patch box2dweb
+(function(box2d: any) {
+	// https://www.ecma-international.org/ecma-262/6.0/#sec-number.epsilon
+	var EPSILON = 2.2204460492503130808472633361816e-16;
+
+	var b2Settings = box2d.Common.b2Settings,
+		b2Body = box2d.Dynamics.b2Body,
+		b2World = box2d.Dynamics.b2World,
+		b2Contact = box2d.Dynamics.Contacts.b2Contact;
+
+	// https://github.com/hecht-software/box2dweb/blob/master/Box2D.js#L6282
+	b2World.prototype.SolveTOI = function (step: any) {
+		var b;
+		var fA;
+		var fB;
+		var bA;
+		var bB;
+		var cEdge;
+		var j;
+		var island = this.m_island;
+		island.Initialize(this.m_bodyCount, b2Settings.b2_maxTOIContactsPerIsland, b2Settings.b2_maxTOIJointsPerIsland, null, this.m_contactManager.m_contactListener, this.m_contactSolver);
+		var queue = b2World.s_queue;
+		for (b = this.m_bodyList;
+			b; b = b.m_next) {
+			b.m_flags &= ~b2Body.e_islandFlag;
+			b.m_sweep.t0 = 0.0;
+		}
+		var c;
+		for (c = this.m_contactList;
+			c; c = c.m_next) {
+			c.m_flags &= ~(b2Contact.e_toiFlag | b2Contact.e_islandFlag);
+		}
+		for (j = this.m_jointList;
+			j; j = j.m_next) {
+			j.m_islandFlag = false;
+		}
+		terminated:
+		for (;;) {
+			if (this.terminator && this.terminator(this)) {
+				break terminated;
+			}
+			var minContact = null;
+			var minTOI = 1.0;
+			for (c = this.m_contactList;
+				c; c = c.m_next) {
+				if (c.IsSensor() == true || c.IsEnabled() == false || c.IsContinuous() == false) {
+					continue;
+				}
+				var toi = 1.0;
+				if (c.m_flags & b2Contact.e_toiFlag) {
+					toi = c.m_toi;
+				}
+				else {
+					fA = c.m_fixtureA;
+					fB = c.m_fixtureB;
+					bA = fA.m_body;
+					bB = fB.m_body;
+					if ((bA.GetType() != b2Body.b2_dynamicBody || bA.IsAwake() == false) && (bB.GetType() != b2Body.b2_dynamicBody || bB.IsAwake() == false)) {
+						continue;
+					}
+					var t0 = bA.m_sweep.t0;
+					if (bA.m_sweep.t0 < bB.m_sweep.t0) {
+						t0 = bB.m_sweep.t0;
+						bA.m_sweep.Advance(t0);
+					}
+					else if (bB.m_sweep.t0 < bA.m_sweep.t0) {
+						t0 = bA.m_sweep.t0;
+						bB.m_sweep.Advance(t0);
+					}
+					toi = c.ComputeTOI(bA.m_sweep, bB.m_sweep);
+					b2Settings.b2Assert(0.0 <= toi && toi <= 1.0);
+					if (toi > 0.0 && toi < 1.0) {
+						toi = (1.0 - toi) * t0 + toi;
+						if (toi > 1) toi = 1;
+					}
+					c.m_toi = toi;
+					c.m_flags |= b2Contact.e_toiFlag;
+				}
+				if (Number.MIN_VALUE < toi && toi < minTOI) {
+					minContact = c;
+					minTOI = toi;
+				}
+			}
+			// Number.MIN_VALUE ではなく EPSILON を用いる。
+			if (minContact == null || 1.0 - 100.0 * EPSILON < minTOI) {
+				break;
+			}
+			fA = minContact.m_fixtureA;
+			fB = minContact.m_fixtureB;
+			bA = fA.m_body;
+			bB = fB.m_body;
+			b2World.s_backupA.Set(bA.m_sweep);
+			b2World.s_backupB.Set(bB.m_sweep);
+			bA.Advance(minTOI);
+			bB.Advance(minTOI);
+			minContact.Update(this.m_contactManager.m_contactListener);
+			minContact.m_flags &= ~b2Contact.e_toiFlag;
+			if (minContact.IsSensor() == true || minContact.IsEnabled() == false) {
+				bA.m_sweep.Set(b2World.s_backupA);
+				bB.m_sweep.Set(b2World.s_backupB);
+				bA.SynchronizeTransform();
+				bB.SynchronizeTransform();
+				continue;
+			}
+			if (minContact.IsTouching() == false) {
+				continue;
+			}
+			var seed = bA;
+			if (seed.GetType() != b2Body.b2_dynamicBody) {
+				seed = bB;
+			}
+			island.Clear();
+			var queueStart = 0;
+			var queueSize = 0;
+			queue[queueStart + queueSize++] = seed;
+			seed.m_flags |= b2Body.e_islandFlag;
+			while (queueSize > 0) {
+				if (this.terminator && this.terminator(this)) {
+					break terminated;
+				}
+				b = queue[queueStart++];
+				--queueSize;
+				island.AddBody(b);
+				if (b.IsAwake() == false) {
+					b.SetAwake(true);
+				}
+				if (b.GetType() != b2Body.b2_dynamicBody) {
+					continue;
+				}
+				for (cEdge = b.m_contactList;
+					cEdge; cEdge = cEdge.next) {
+					if (island.m_contactCount == island.m_contactCapacity) {
+						break;
+					}
+					if (cEdge.contact.m_flags & b2Contact.e_islandFlag) {
+						continue;
+					}
+					if (cEdge.contact.IsSensor() == true || cEdge.contact.IsEnabled() == false || cEdge.contact.IsTouching() == false) {
+						continue;
+					}
+					island.AddContact(cEdge.contact);
+					cEdge.contact.m_flags |= b2Contact.e_islandFlag;
+					var other = cEdge.other;
+					if (other.m_flags & b2Body.e_islandFlag) {
+						continue;
+					}
+					if (other.GetType() != b2Body.b2_staticBody) {
+						other.Advance(minTOI);
+						other.SetAwake(true);
+					}
+					queue[queueStart + queueSize] = other;
+					++queueSize;
+					other.m_flags |= b2Body.e_islandFlag;
+				}
+				for (var jEdge = b.m_jointList; jEdge; jEdge = jEdge.next) {
+					if (island.m_jointCount == island.m_jointCapacity) continue;
+					if (jEdge.joint.m_islandFlag == true) continue;
+					other = jEdge.other;
+					if (other.IsActive() == false) {
+						continue;
+					}
+					island.AddJoint(jEdge.joint);
+					jEdge.joint.m_islandFlag = true;
+					if (other.m_flags & b2Body.e_islandFlag) continue;
+					if (other.GetType() != b2Body.b2_staticBody) {
+						other.Advance(minTOI);
+						other.SetAwake(true);
+					}
+					queue[queueStart + queueSize] = other;
+					++queueSize;
+					other.m_flags |= b2Body.e_islandFlag;
+				}
+			}
+			var subStep = b2World.s_timestep;
+			subStep.warmStarting = false;
+			subStep.dt = (1.0 - minTOI) * step.dt;
+			subStep.inv_dt = 1.0 / subStep.dt;
+			subStep.dtRatio = 0.0;
+			subStep.velocityIterations = step.velocityIterations;
+			subStep.positionIterations = step.positionIterations;
+			island.SolveTOI(subStep);
+			var i = 0;
+			for (i = 0;
+				i < island.m_bodyCount; ++i) {
+				b = island.m_bodies[i];
+				b.m_flags &= ~b2Body.e_islandFlag;
+				if (b.IsAwake() == false) {
+					continue;
+				}
+				if (b.GetType() != b2Body.b2_dynamicBody) {
+					continue;
+				}
+				b.SynchronizeFixtures();
+				for (cEdge = b.m_contactList;
+					cEdge; cEdge = cEdge.next) {
+					cEdge.contact.m_flags &= ~b2Contact.e_toiFlag;
+				}
+			}
+			for (i = 0;
+				i < island.m_contactCount; ++i) {
+				c = island.m_contacts[i];
+				c.m_flags &= ~(b2Contact.e_toiFlag | b2Contact.e_islandFlag);
+			}
+			for (i = 0;
+				i < island.m_jointCount; ++i) {
+				j = island.m_joints[i];
+				j.m_islandFlag = false;
+			}
+			this.m_contactManager.FindNewContacts();
+		}
+	};
+
+	// b2World を継承するコンストラクタ関数
+	function ctor() {
+		b2World.apply(this, arguments);
+		this.terminator = null;
+	}
+	ctor.prototype = Object.create(b2World.prototype);
+
+	box2d.Dynamics.b2World = ctor;
+})(b2);
+/* tslint:enable */
+
 /**
  * AkashicのエンティティをBox2DWebのb2Worldに追加し、演算結果をエンティティに反映するクラス。
  */
@@ -37,6 +261,15 @@ export class Box2D implements g.Destroyable {
 		this.world = new b2.Dynamics.b2World(new b2.Common.Math.b2Vec2(param.gravity[0], param.gravity[1]), sleep);
 		this.bodies = [];
 		this.scale = param.scale;
+	}
+
+	/**
+	 * 物理演算を中断させるコールバック関数を登録する。
+	 * コールバック関数が true を返すとき、物理演算を中断する。
+	 * @param callback コールバック関数。null または undefined のとき、すでに登録されたコールバック関数があればそれを削除する。
+	 */
+	setTerminator(callback: (world: b2.Dynamics.b2World) => void): void {
+		(this.world as any).terminator = callback;
 	}
 
 	/**
