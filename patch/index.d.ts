55b4ae40c8c6196a1bf9464a4fb53e4ce34e6ad0
BlackSpotBear <blackspotbear@users.noreply.github.com>
2019-01-09 09:50:26 +0900

    Rename Options PatchBox2DOptions

--- patch/index.d.ts
+++ patch/index.d.ts
@@ -4,7 +4,7 @@ import * as Box2D from "../lib";
 /**
  * パッチのオプショナルなパラメータ。
  */
-export interface Options {
+export interface PatchBox2DOptions {
     /**
      * 最小TOI算出ループの繰り返し回数の上限。
      * 指定しないとき、Number.MAX_SAFE_INTEGER。
@@ -17,9 +17,9 @@ export interface Options {
  *
  * 以下の修正・機能追加を行う。
  * - 無限ループに陥る不具合の修正。
- * - 物理計算の時間を制限するため、最小TOI算出ループの繰り返し回数の上限を設定する機能の追加。
+ * - 物理計算の時間を制限するため、最小TOI算出ループの繰り返し回数の上限を設定する機能の追加。
  *
  * @param box2d Box2D。
  * @param opts オプション。
  */
-export function patchBox2D(box2d: Box2D.Box2D, opts?: Options);
+export function patchBox2D(box2d: Box2D.Box2D, opts?: PatchBox2DOptions);
