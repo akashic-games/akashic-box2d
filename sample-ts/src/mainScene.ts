9be71440e6ea3889f041422397f21fce1cd2e55e
yusuke-ogiwara <yusuke_ogiwara@dwango.co.jp>
2019-05-07 14:30:12 +0900

    Follow sample-ts to next-version

--- sample-ts/src/mainScene.ts
+++ sample-ts/src/mainScene.ts
@@ -1,4 +1,4 @@
-import * as Box2D from "@akashic-extension/akashic-box2d";
+import * as box2d from "@akashic-extension/akashic-box2d";
 var game = g.game;
 
 export = () => {
@@ -11,7 +11,7 @@ export = () => {
 			scale: 50,
 			sleep: true
 		};
-		const b2 = new Box2D.Box2D(worldOption);
+		const b2 = new box2d.Box2D(worldOption);
 
 		// 地面エンティティの生成
 		const floor = new g.FilledRect({
@@ -33,9 +33,8 @@ export = () => {
 		});
 
 		// 地面エンティティを静的物体化
-		const staticDef = b2.createBodyDef({
-			type: Box2D.BodyType.Static
-		});
+		const staticDef = new box2d.b2BodyDef();
+		staticDef.type = box2d.b2BodyType.b2_staticBody;
 
 		// Box2Dに地面エンティティを追加
 		const floorBody = b2.createBody(floor, staticDef, floorDef);
@@ -44,10 +43,10 @@ export = () => {
 		const rect1 = new g.FilledRect({
 			scene: scene,
 			cssColor: "pink",
-			x: 100,
-			y: 100,
-			width: 10,
-			height: 90
+			x: 200,
+			y: 200,
+			width: 20,
+			height: 180
 		});
 		scene.append(rect1);
 
@@ -55,10 +54,10 @@ export = () => {
 		const rect2 = new g.FilledRect({
 			scene: scene,
 			cssColor: "red",
-			x: 120,
+			x: 240,
 			y: 0,
-			width: 30,
-			height: 10
+			width: 60,
+			height: 20
 		});
 		scene.append(rect2);
 
@@ -66,10 +65,10 @@ export = () => {
 		const soccer = new g.Sprite({
 			scene: scene,
 			src: scene.assets["soccer"],
-			x: 105,
+			x: 210,
 			y: 30,
-			width: 40,
-			height: 40,
+			width: 80,
+			height: 80,
 			srcWidth: 100,
 			srcHeight: 98
 		});
@@ -79,75 +78,84 @@ export = () => {
 		const pentagon = new g.Sprite({
 			scene: scene,
 			src: scene.assets["pentagon"],
-			x: 150,
+			x: 260,
 			y: 0,
-			width: 40,
-			height: 40,
+			width: 80,
+			height: 80,
 			srcWidth: 100,
 			srcHeight: 95
 		});
 		scene.append(pentagon);
 
-		// エンティティの性質を定義
-		const entityDef = b2.createFixtureDef({
+		// エンティティの共通の性質
+		const commonFixtureDef = {
 			density: 1.0, // 密度
 			friction: 0.5, // 摩擦係数
 			restitution: 0.3 // 反発係数
-		});
+		};
 
 		// 動的物体化
-		const dynamicDef = b2.createBodyDef({
-			type: Box2D.BodyType.Dynamic
-		});
+		const dynamicDef = new box2d.b2BodyDef();
+		dynamicDef.type = box2d.b2BodyType.b2_dynamicBody;
 
-		// rect1エンティティを四角に設定
-		entityDef.shape = b2.createRectShape(rect1.width, rect1.height);
 		// rect1エンティティをBox2Dに追加
-		b2.createBody(rect1, dynamicDef, entityDef);
+		b2.createBody(
+			rect1, dynamicDef,
+			b2.createFixtureDef({
+				shape: b2.createRectShape(rect1.width, rect1.height), // rect1エンティティを四角に設定
+				...commonFixtureDef
+			})
+		);
 
-		// rect2エンティティを四角に設定
-		entityDef.shape = b2.createRectShape(rect2.width, rect2.height);
 		// rect2エンティティをBox2Dに追加
-		b2.createBody(rect2, dynamicDef, entityDef);
+		b2.createBody(
+			rect2, dynamicDef,
+			b2.createFixtureDef({
+				shape: b2.createRectShape(rect2.width, rect2.height), // rect2エンティティを四角に設定
+				...commonFixtureDef
+			})
+		);
 
-		// サッカーボールエンティティを円に設定
-		entityDef.shape = b2.createCircleShape(soccer.width);
 		// サッカーボールエンティティをBox2Dに追加
-		const soccerBody = b2.createBody(soccer, dynamicDef, entityDef);
+		const soccerBody = b2.createBody(
+			soccer, dynamicDef,
+			b2.createFixtureDef({
+				shape: b2.createCircleShape(soccer.width), // サッカーボールエンティティを円に設定
+				...commonFixtureDef
+			})
+		);
 
-		// 五角形エンティティを設定
+		// 五角形エンティティの頂点を設定
 		const vertices = [
-			b2.vec2(20 - pentagon.width / 2, 0 - pentagon.height / 2),
-			b2.vec2(pentagon.width - pentagon.width / 2, 14 - pentagon.height / 2),
-			b2.vec2(32 - pentagon.width / 2, pentagon.height - pentagon.height / 2),
-			b2.vec2(8 - pentagon.width / 2, pentagon.height - pentagon.height / 2),
-			b2.vec2(0 - pentagon.width / 2, 14 - pentagon.height / 2)
+			b2.vec2(40 - pentagon.width / 2, 0 - pentagon.height / 2),
+			b2.vec2(pentagon.width - pentagon.width / 2, 28 - pentagon.height / 2),
+			b2.vec2(64 - pentagon.width / 2, pentagon.height - pentagon.height / 2),
+			b2.vec2(16 - pentagon.width / 2, pentagon.height - pentagon.height / 2),
+			b2.vec2(0 - pentagon.width / 2, 28 - pentagon.height / 2)
 		];
-		entityDef.shape = b2.createPolygonShape(vertices);
 
 		// 五角形エンティティをBox2Dに追加
-		b2.createBody(pentagon, dynamicDef, entityDef);
+		b2.createBody(
+			pentagon, dynamicDef,
+			b2.createFixtureDef({
+				shape: b2.createPolygonShape(vertices)
+			})
+		);
 
-		// 接触イベントのリスナーを生成
-		const contactListener = new Box2D.Box2DWeb.Dynamics.b2ContactListener;
+		const contactManager = new box2d.ContactManager({ box2d: b2 });
+
 		// 接触開始時のイベントリスナー
-		contactListener.BeginContact = (contact: Box2D.Box2DWeb.Dynamics.Contacts.b2Contact) => {
+		contactManager.createBeginContactTrigger(soccerBody, floorBody).add(() => {
 			// サッカーボールと地面がぶつかったら地面の色を青にする
-			if (b2.isContact(floorBody, soccerBody, contact)) {
-				floor.cssColor = "blue";
-				floor.modified();
-			}
-		};
+			floor.cssColor = "blue";
+			floor.modified();
+		});
 		// 接触が離れた時のイベントリスナー
-		contactListener.EndContact = (contact: Box2D.Box2DWeb.Dynamics.Contacts.b2Contact) => {
+		contactManager.createEndContactTrigger(soccerBody, floorBody).add(() => {
 			// サッカーボールと地面が離れたら地面の色を黒にする
-			if (b2.isContact(floorBody, soccerBody, contact)) {
-				floor.cssColor = "black";
-				floor.modified();
-			}
-		};
-		// イベントリスナーを設定
-		b2.world.SetContactListener(contactListener);
+			floor.cssColor = "black";
+			floor.modified();
+		});
 
 		soccer.touchable = true;
 		soccer.pointDown.add((o: g.PointDownEvent) => {
@@ -157,8 +165,8 @@ export = () => {
 				y: o.point.y - soccer.height / 2
 			};
 			// ボールクリックでインパルスを与える
-			// ApplyImpulse()はBox2Dの機能です。
-			soccerBody.b2body.ApplyImpulse(b2.vec2(delta.x * -5, delta.y * -5), b2.vec2(pos.x, pos.y));
+			// ApplyLinearImpulse()は Box2D.ts の機能です。
+			soccerBody.b2Body.ApplyLinearImpulse(b2.vec2(delta.x * -20, delta.y * -20), b2.vec2(pos.x, pos.y));
 		});
 
 		scene.update.add(() => {
